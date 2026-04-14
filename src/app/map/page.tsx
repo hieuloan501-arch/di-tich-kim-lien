cd9d876525464d079155b8db8c805dc8adbae66b
Nguyen My <nguyenmy4@gmail.com>
2026-04-15 02:01:51 +0700

    Fix: Add use client to map page

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,6 @@
+"use client"; //
 import dynamic from 'next/dynamic';
-
+ 
 const MapComponent = dynamic(() => import('./MapComponent'), { 
   ssr: false, // Tắt render phía server để tránh lỗi appendChild
   loading: () => <div className="h-screen bg-slate-950 flex items-center justify-center text-white">Đang tải bản đồ...</div>
